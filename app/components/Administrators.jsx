d19a884d7f567f43a657c9a08c0516d46ba159f5
Joshua <koffworld@gmail.com>
2025-03-02 09:57:53 +0000

    ✨ feat: admins populated

--- app/components/Administrators.jsx
+++ app/components/Administrators.jsx
@@ -1,7 +1,79 @@
+"use client"
 import { Table } from "antd"
-import React from "react"
+import React, { useEffect, useState } from "react"
+import { HiOutlineDotsVertical } from "react-icons/hi"
 
 const Administrators = () => {
+  const [admins, setAdmins] = useState([])
+
+  // get all admins
+  const getAllAdmins = async () => {
+    try {
+      const requestOptions = {
+        method: "GET",
+        redirect: "follow",
+      }
+
+      await fetch("https://api.kofflabs.com/api/v1/admins/all", requestOptions)
+        .then((response) => response.json())
+        .then((result) => {
+          setAdmins(result?.admins)
+        })
+        .catch((error) => console.error(error))
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  // init
+  useEffect(() => {
+    getAllAdmins()
+  }, [])
+
+  // columns
+  const columns = [
+    {
+      title: "#",
+      render: (_value, _record, index) => <p>{index + 1}</p>,
+    },
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+      render: (_, record) => (
+        <div className="flex items-center">
+          <p className="capitalize">
+            {record.firstName} {record.lastName}
+          </p>
+        </div>
+      ),
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      key: "email",
+      render: (_, record) => <p className="lowercase">{record?.email}</p>,
+    },
+    {
+      title: "Status",
+      dataIndex: "isBlocked",
+      key: "isBlocked",
+      render: (_, record) => (
+        <p className={`text-${record.status === "active" ? "green" : "red"}`}>
+          {record?.isBlocked ? "Blocked" : "Active"}
+        </p>
+      ),
+    },
+    {
+      title: "Actions",
+      render: (_, record) => (
+        <div>
+          <HiOutlineDotsVertical />
+        </div>
+      ),
+    },
+  ]
+
   return (
     <div className="bg-white w-full p-5 rounded-lg shadow">
       {/* header */}
@@ -13,7 +85,7 @@ const Administrators = () => {
       </div>
 
       {/* content */}
-      <Table className="mt-5" />
+      <Table className="mt-5" columns={columns} dataSource={admins} />
     </div>
   )
 }

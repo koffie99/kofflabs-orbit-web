0655722e44a7a76bdb5c91a3594b7634df50f407
Joshua <koffworld@gmail.com>
2025-03-02 12:06:22 +0000

    ✨ feat: department crud added

--- app/components/Departments.jsx
+++ app/components/Departments.jsx
@@ -1,19 +1,32 @@
 "use client"
-import { Modal, Table } from "antd"
+import { Modal, Popconfirm, Table } from "antd"
 import Image from "next/image"
 import React, { useEffect, useState } from "react"
-import { ToastContainer, toast } from "react-toastify"
+// import { ToastContainer, toast } from "react-toastify"
+import { Toaster, toast } from "react-hot-toast"
+import { CiEdit } from "react-icons/ci"
+import { GoTrash } from "react-icons/go"
 
 const Departments = () => {
   const [departments, setDepartments] = useState([])
   const [openAddModal, setOpenAddModal] = useState(false)
+  const [openUpdateModal, setOpenUpdateModal] = useState(false)
   const [deptName, setDeptName] = useState("")
   const [adding, setAdding] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const [selectedDeptName, setSelectedDeptName] = useState("")
+  const [selectedDeptId, setSelectedDeptId] = useState("")
+  const [newDeptName, setNewDeptName] = useState("")
+
+  // reset new dept name
+  useEffect(() => {
+    setNewDeptName(selectedDeptName)
+  }, [selectedDeptName])
 
   // handle add department
   const addDepartment = async () => {
     if (!deptName) {
-      toast.error("Please enter deptarment name")
+      toast.error("Please enter department name")
       setAdding(false)
     } else {
       try {
@@ -78,6 +91,74 @@ const Departments = () => {
     }
   }
 
+  // delete a department
+  const deleteDepartment = async (deptId) => {
+    try {
+      const requestOptions = {
+        method: "DELETE",
+        redirect: "follow",
+      }
+
+      await fetch(
+        `https://api.kofflabs.com/api/v1/departments/delete/${deptId}`,
+        requestOptions
+      )
+        .then((response) => response.json())
+        .then((result) => {
+          if (result.msg === "department deleted successfully") {
+            toast.success("Department deleted successfully")
+            getAllDepartments()
+          } else {
+            toast.error(result.msg)
+          }
+        })
+        .catch((error) => console.error(error))
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  // update a department
+  const updateDepartment = async () => {
+    try {
+      setLoading(true)
+      const myHeaders = new Headers()
+      myHeaders.append("Content-Type", "application/json")
+
+      const raw = JSON.stringify({
+        name: newDeptName.trim().toLowerCase(),
+      })
+
+      const requestOptions = {
+        method: "PUT",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow",
+      }
+
+      await fetch(
+        `https://api.kofflabs.com/api/v1/departments/update/${selectedDeptId}`,
+        requestOptions
+      )
+        .then((response) => response.json())
+        .then((result) => {
+          if (result.msg === "department updated successfully") {
+            getAllDepartments()
+            setLoading(false)
+            toast.success("Department updated successfully")
+            setOpenUpdateModal(false)
+          } else {
+            toast.error(result.msg)
+            setLoading(false)
+          }
+        })
+        .catch((error) => console.error(error))
+    } catch (err) {
+      console.log(err)
+      setLoading(false)
+    }
+  }
+
   // init
   useEffect(() => {
     getAllDepartments()
@@ -118,15 +199,28 @@ const Departments = () => {
       key: "actions",
       render: (text, record) => (
         <div className="flex gap-x-2">
-          <button className="bg-[#f29235] text-white py-2 px-3 rounded-md text-sm">
-            Edit
-          </button>
           <button
-            className="bg-red-500 text-white py-2 px-3 rounded-md text-sm"
-            onClick={() => handleDelete(record._id)}
+            className="bg-[#b5eaff] text-[#2587ad] py-2 px-3 rounded-md text-sm"
+            onClick={() => {
+              setSelectedDeptName(record.name)
+              setSelectedDeptId(record._id)
+              setOpenUpdateModal(true)
+            }}
           >
-            Delete
+            <CiEdit />
           </button>
+          <Popconfirm
+            title={`Wanna delete ${record.name}`}
+            okText="Delete Department"
+            okButtonProps={{
+              style: { backgroundColor: "tomato", color: "white" },
+            }}
+            onConfirm={() => deleteDepartment(record._id)}
+          >
+            <button className="bg-[#ffcfc8] text-[#d45139] py-1 px-3 rounded-md text-sm">
+              <GoTrash />
+            </button>
+          </Popconfirm>
         </div>
       ),
     },
@@ -180,7 +274,39 @@ const Departments = () => {
         </div>
       </Modal>
 
-      <ToastContainer />
+      {/* update model */}
+      <Modal
+        open={openUpdateModal}
+        title="Update Department"
+        onCancel={() => setOpenUpdateModal(false)}
+        footer={false}
+      >
+        <div>
+          <input
+            type="text"
+            className="ring-1 ring-[#ccc] py-2 px-2 rounded-lg w-full capitalize"
+            onChange={(e) => setNewDeptName(e.target.value)}
+            value={newDeptName}
+          />
+          <button
+            className="bg-[#f29235] text-white mt-2 p-2 rounded-lg text flex items-center justify-center w-full"
+            onClick={() => updateDepartment()}
+          >
+            {loading ? (
+              <Image
+                width={30}
+                height={30}
+                alt="loading anim"
+                src="/gifs/whiteloading.gif"
+              />
+            ) : (
+              "Update Department"
+            )}
+          </button>
+        </div>
+      </Modal>
+
+      <Toaster />
     </div>
   )
 }

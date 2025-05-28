1cb040b1b31a09b6ee51460476c545ca1fa4dde9
Joshua <koffworld@gmail.com>
2025-05-28 10:43:04 +0000

    👌 orbit dashboard goes dark theme

--- app/components/Dashboard.jsx
+++ app/components/Dashboard.jsx
@@ -33,9 +33,9 @@ const Dashboard = () => {
   }, [])
 
   return (
-    <div className="bg-white w-full p-5 rounded-lg shadow">
+    <div className="bg-[#131313] w-full p-5 rounded-lg shadow">
       <div>
-        <h2 className="font-semibold text-xl">Dashboard</h2>
+        <h2 className="font-semibold text-xl text-neutral-300">Dashboard</h2>
       </div>
 
       {/* stats */}
--- app/uibits/DashCard.jsx
+++ app/uibits/DashCard.jsx
@@ -3,13 +3,13 @@ import CountUp from "react-countup"
 
 const DashCard = ({ Icon, color, desc, value }) => {
   return (
-    <div className="bg-[#fff5ec] flex items-center gap-3 p-6 rounded-lg">
+    <div className="bg-[#1b1b1b] flex items-center gap-3 p-6 rounded-lg shadow-2xl">
       <div>{Icon && <Icon className="text-2xl text-[#f39136]" />}</div>
       <div>
         <h3 className="text-xl text-[#f39136] font-bold">
           <CountUp end={value} duration={2.5} separator="," />
         </h3>
-        <p>{desc}</p>
+        <p className="text-neutral-400 text-sm">{desc}</p>
       </div>
     </div>
   )

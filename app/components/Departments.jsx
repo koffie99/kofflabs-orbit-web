8ac470fdd529fa1d617656e022d4ddf68d4271b6
Joshua <koffworld@gmail.com>
2025-05-28 11:16:10 +0000

    ✨ departments go dark theme

--- app/components/Departments.jsx
+++ app/components/Departments.jsx
@@ -1,5 +1,5 @@
 "use client"
-import { Modal, Popconfirm, Table } from "antd"
+import { Modal, Popconfirm, Table, ConfigProvider, theme } from "antd"
 import Image from "next/image"
 import React, { useEffect, useState } from "react"
 // import { ToastContainer, toast } from "react-toastify"
@@ -225,7 +225,7 @@ const Departments = () => {
   ]
 
   return (
-    <div className="bg-white w-full p-5 rounded-lg shadow">
+    <div className="bg-[#131313] w-full p-5 rounded-lg shadow">
       {/* header */}
       <div className="flex items-center justify-between">
         <div>
@@ -243,7 +243,49 @@ const Departments = () => {
       </div>
 
       {/* content */}
-      <Table className="mt-5" columns={columns} dataSource={departments} />
+      <ConfigProvider
+                      theme={{
+                        algorithm: theme.darkAlgorithm,
+                        token: {
+                          colorPrimary: '#08807a',
+                          colorBgContainer: '#181818',
+                          colorBgElevated: '#181818',
+                          colorBgLayout: '#181818',
+                          colorBgSpotlight: '#181818',
+                          colorBgFloating: '#181818',
+                          colorBgSecondary: '#181818',
+                          colorBgSecondaryHover: '#181818',
+                          colorBgSecondaryActive: '#181818',
+                          colorBorder: '#2d2d2d',
+                          colorBorderSecondary: '#2d2d2d',
+                          colorBorderTertiary: '#2d2d2d',
+                          colorBorderQuaternary: '#2d2d2d',
+                          colorBorderHover: '#2d2d2d',
+                          colorBorderActive: '#2d2d2d',
+                          colorBorderSelected: '#2d2d2d',
+                          colorBorderSelectedHover: '#2d2d2d',
+                          colorBorderSelectedActive: '#2d2d2d',
+                          colorBorderDisabled: '#2d2d2d',
+                          colorBorderDisabledHover: '#2d2d2d',
+                          colorBorderDisabledActive: '#2d2d2d',
+                          colorText: '#ffffff',
+                          colorTextSecondary: '#ffffff',
+                          colorTextTertiary: '#ffffff',
+                          colorTextQuaternary: '#ffffff',
+                          colorTextPlaceholder: '#ffffff',
+                          colorTextDisabled: '#ffffff',
+                          colorTextHeading: '#ffffff',
+                          colorTextTitle: '#ffffff',
+                          colorTextDescription: '#ffffff',
+                          colorTextLightSolid: '#ffffff',
+                          colorTextLight: '#ffffff',
+                          colorTextMuted: '#ffffff',
+                          colorTextLighter: '#ffffff'
+                        }
+                      }}
+                    >
+          <Table className="mt-5" columns={columns} dataSource={departments} />
+      </ConfigProvider>
 
       {/* add department modal */}
       <Modal

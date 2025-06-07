043dbaae273e4d8a6f3dd7c5694783d987cd725f
Joshua <koffworld@gmail.com>
2025-06-07 19:02:23 +0000

    🌟 letter writing flow optimized

--- app/components/Letters.jsx
+++ app/components/Letters.jsx
@@ -7,6 +7,8 @@ import { FiEye } from "react-icons/fi";
 import { FiEdit3 } from "react-icons/fi";
 import { GoTrash } from "react-icons/go";
 import EntityLength from "../uibits/EntityLength";
+import { FiExternalLink } from "react-icons/fi";
+import {motion} from 'motion/react'
 
 const Letters = () => {
   const [letters, setLetters] = useState([]);
@@ -56,7 +58,7 @@ const Letters = () => {
         .then((result) => {
           if (result.msg === "letter created successfully") {
             getAllLetters();
-            setLetterLink(result.pdfUrl);
+            setLetterLink(result?.pdfUrl);
             toast.success("Letter written successfully");
 
             setOpenAddLetterModal(false);
@@ -233,6 +235,48 @@ const Letters = () => {
         />
       </ConfigProvider>
       {/* write letter modal */}
+
+      <ConfigProvider
+        theme={{
+          algorithm: theme.darkAlgorithm,
+          token: {
+            colorPrimary: "#08807a",
+            colorBgContainer: "#181818",
+            colorBgElevated: "#181818",
+            colorBgLayout: "#181818",
+            colorBgSpotlight: "#181818",
+            colorBgFloating: "#181818",
+            colorBgSecondary: "#181818",
+            colorBgSecondaryHover: "#181818",
+            colorBgSecondaryActive: "#181818",
+            colorBorder: "#2d2d2d",
+            colorBorderSecondary: "#2d2d2d",
+            colorBorderTertiary: "#2d2d2d",
+            colorBorderQuaternary: "#2d2d2d",
+            colorBorderHover: "#2d2d2d",
+            colorBorderActive: "#2d2d2d",
+            colorBorderSelected: "#2d2d2d",
+            colorBorderSelectedHover: "#2d2d2d",
+            colorBorderSelectedActive: "#2d2d2d",
+            colorBorderDisabled: "#2d2d2d",
+            colorBorderDisabledHover: "#2d2d2d",
+            colorBorderDisabledActive: "#2d2d2d",
+            colorText: "#ffffff",
+            colorTextSecondary: "#ffffff",
+            colorTextTertiary: "#ffffff",
+            colorTextQuaternary: "#ffffff",
+            colorTextPlaceholder: "#ffffff",
+            colorTextDisabled: "#ffffff",
+            colorTextHeading: "#ffffff",
+            colorTextTitle: "#ffffff",
+            colorTextDescription: "#ffffff",
+            colorTextLightSolid: "#ffffff",
+            colorTextLight: "#ffffff",
+            colorTextMuted: "#ffffff",
+            colorTextLighter: "#ffffff",
+          },
+        }}
+      >
       <Modal
         title="Write a letter"
         open={openAddLetterModal}
@@ -320,20 +364,75 @@ const Letters = () => {
           </button>
         </div>
       </Modal>
+      </ConfigProvider>
 
       {/* generated letter page */}
+      <ConfigProvider
+        theme={{
+          algorithm: theme.darkAlgorithm,
+          token: {
+            colorPrimary: "#08807a",
+            colorBgContainer: "#181818",
+            colorBgElevated: "#181818",
+            colorBgLayout: "#181818",
+            colorBgSpotlight: "#181818",
+            colorBgFloating: "#181818",
+            colorBgSecondary: "#181818",
+            colorBgSecondaryHover: "#181818",
+            colorBgSecondaryActive: "#181818",
+            colorBorder: "#2d2d2d",
+            colorBorderSecondary: "#2d2d2d",
+            colorBorderTertiary: "#2d2d2d",
+            colorBorderQuaternary: "#2d2d2d",
+            colorBorderHover: "#2d2d2d",
+            colorBorderActive: "#2d2d2d",
+            colorBorderSelected: "#2d2d2d",
+            colorBorderSelectedHover: "#2d2d2d",
+            colorBorderSelectedActive: "#2d2d2d",
+            colorBorderDisabled: "#2d2d2d",
+            colorBorderDisabledHover: "#2d2d2d",
+            colorBorderDisabledActive: "#2d2d2d",
+            colorText: "#ffffff",
+            colorTextSecondary: "#ffffff",
+            colorTextTertiary: "#ffffff",
+            colorTextQuaternary: "#ffffff",
+            colorTextPlaceholder: "#ffffff",
+            colorTextDisabled: "#ffffff",
+            colorTextHeading: "#ffffff",
+            colorTextTitle: "#ffffff",
+            colorTextDescription: "#ffffff",
+            colorTextLightSolid: "#ffffff",
+            colorTextLight: "#ffffff",
+            colorTextMuted: "#ffffff",
+            colorTextLighter: "#ffffff",
+          },
+        }}
+      >
       <Modal
         // title="Generated Letter"
         open={openLetterModal}
         onCancel={() => setOpenLetterModal(false)}
         footer={null}
+        style={{ width: "700px" }}
       >
+        <div className="flex flex-col gap-5 items-center">
         {writing ? (
           "Writing letter..."
         ) : (
-          <iframe src={letterLink} className="w-full h-[80vh]"></iframe>
+          <iframe src={letterLink} className="w-full h-[90vh]"></iframe>
         )}
+        {/* open in a new tab button with icon */}
+        <motion.button
+          whileTap={{ scale: 0.9 }}
+          className="p-2 bg-[#f39136] text-white rounded-md mt-5 display flex items-center gap-3"
+          onClick={() => window.open(letterLink, "_blank")}
+        >
+          <FiExternalLink className="text-xl" />
+          Open in a new tab
+        </motion.button>
+        </div>
       </Modal>
+      </ConfigProvider>
 
       <Toaster />
     </div>
